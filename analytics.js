310cd9b321054ec257810e2bfa8ce847c635b1dc
Rogerio Henrique Berlanda <rogerio.henrique@maristabrasil.org>
2026-07-31 15:11:50 -0300

    feat: adiciona vercel speed insights

--- analytics.js
+++ analytics.js
@@ -1,8 +1,10 @@
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
-// Inicialização automática do Vercel Web Analytics
+// Inicialização automática do Vercel Web Analytics e Speed Insights
 try {
   inject();
+  injectSpeedInsights();
 } catch (e) {
   // Ignora erros em desenvolvimento local offline
 }
